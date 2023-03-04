64d63f6cc3322e69b5b460eaa44fa00f62f5a8f0
prudhvi1601 <prudhvi1601@gmail.com>
2023-03-04 13:11:48 +0530

    li added in ol

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//your code here!
 
+const ol = document.querySelector('#infi-list');
+
+const li = document.createElement('li');
+function createItem() {
+    for (var i = 0; i < 9; i++) {
+        li.innerHTML = "Item " + i;
+        ol.appendChild(li);
+    }
+}
+
+console.log(createItem());
